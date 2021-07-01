b61b0fa3490d3ff7ce701b403109133acf35c4f6
davidebortoloni <davidebortoloni@gmail.com>
2021-07-01 16:53:49 +0200

    Correzione dettagli [esercizio finito]

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-var kilometres = prompt('Inserire quanti km dovranno essere percorsi');
+var kilometres = parseFloat(prompt('Inserire quanti km dovranno essere percorsi'));
+
 if (isNaN(kilometres) || kilometres <= 0) {
     alert('Inserire un numero di km valido');
     window.location.reload();
 } else {
-    var userAge = prompt('Inserire l\'età');
+    var userAge = parseInt(Math.floor(prompt('Inserire l\'età')));
     if (isNaN(userAge) || userAge <= 0) {
         alert('Inserire un\'età valida');
         window.location.reload();
@@ -15,12 +16,12 @@ var price = kilometres * priceConstant;
 
 if (userAge < 18) {
     price = price - (price / 100 * 20);
-    price = Math.round(price * 100) / 100;
 }
 if (userAge >= 65) {
     price = price - (price / 100 * 40);
-    price = Math.round(price * 100) / 100;
 }
+
+price = Math.round(price * 100) / 100;
 
 messageId = document.getElementById('ticket-price');
 messageId.innerText = "Ecco il costo del suo biglietto: " + price + "€";
